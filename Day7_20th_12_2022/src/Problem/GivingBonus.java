a9ff583b08ae1c84dedaa3350ef425f84323b0fc
Rahul <Rahul@LAPTOP-0PTDR9UB.mshome.net>
2023-01-01 21:30:21 +0530

    commit

---
+++ Day7_20th_12_2022/src/Problem/GivingBonus.java
@@ -0,0 +1,29 @@
+package Problem;
+
+import java.util.Scanner;
+
+public class GivingBonus {
+
+	
+	public static void main(String[] args) {
+		float joiningYear,currentYear,serving_year;
+		String name;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the name of the employee: ");
+        name = sc.nextLine();
+		System.out.println("Enter year of joining: ");
+        joiningYear = sc.nextFloat();
+        System.out.println("Enter current year: ");
+        currentYear = sc.nextFloat();
+       
+        serving_year=currentYear-joiningYear;
+        if(serving_year>3) {
+        	System.out.println(name +" is eligible for bonus, given 3000Rs ");
+        }
+        else {
+        	System.out.println(name +" is not eligible for bonus");
+        }
+        
+	}
+}
+
